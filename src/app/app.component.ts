0c041e3f127f3573e675dbd26697a16ce8e882e8
Ahmed Mourad <AMourad@healthinsightsgroup.com>
2025-08-15 04:05:07 +0300

    merg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { RouterModule, Router, NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { filter, Subscription } from 'rxjs';
 
 import { HeaderComponent } from './pages/header/header.component';
 import { FooterComponent } from './pages/footer/footer.component';
@@ -9,12 +11,45 @@ import { FooterComponent } from './pages/footer/footer.component';
   standalone: true,
   imports: [
     RouterModule,
+    CommonModule,
     HeaderComponent,
     FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'HPC Sprint Hackathon';
+  private routerSubscription?: Subscription;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.routerSubscription = this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.updateBodyClass();
+    });
+    
+    // Initial check
+    this.updateBodyClass();
+  }
+
+  ngOnDestroy() {
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
+    }
+  }
+
+  get isDashboardRoute(): boolean {
+    return this.router.url.startsWith('/dashboard');
+  }
+
+  private updateBodyClass() {
+    if (this.isDashboardRoute) {
+      document.body.classList.add('dashboard-route');
+    } else {
+      document.body.classList.remove('dashboard-route');
+    }
+  }
 }
